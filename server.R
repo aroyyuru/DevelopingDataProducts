74883bf6f07c3406fa7e0e8641c748ce60ea5906
aroyyuru <royyuruabhishek@gmail.com>
2016-02-13 17:23:51 -0500

    CourseProject

---
+++ server.R
@@ -0,0 +1,24 @@
+# server.R file for the shiny app
+# App for mileage calculation and cars comparison, 
+# using mtcars dataset, from [R]
+
+
+library(shiny)
+library(datasets)
+library(dplyr)
+
+shinyServer(function(input, output) {
+  output$table <- renderDataTable({
+    disp_seq <- seq(from = input$disp[1], to = input$disp[2], by = 0.1)
+    hp_seq <- seq(from = input$hp[1], to = input$hp[2], by = 1)
+    data <- transmute(mtcars, Car = rownames(mtcars), MilesPerGallon = mpg, 
+                      GasolineExpenditure = input$dis/mpg*input$cost,
+                      Cylinders = cyl, Displacement = disp, Horsepower = hp, 
+                      Transmission = am)
+    data <- filter(data, GasolineExpenditure <= input$gas, Cylinders %in% input$cyl, 
+                   Displacement %in% disp_seq, Horsepower %in% hp_seq, Transmission %in% input$am)
+    data <- mutate(data, Transmission = ifelse(Transmission==0, "Automatic", "Manual"))
+    data <- arrange(data, GasolineExpenditure)
+    data
+  }, options = list(lengthMenu = c(5, 15, 30), pageLength = 30))
+})
